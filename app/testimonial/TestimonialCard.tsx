8e397ecdea72455949a7d712ef507e3071bfda10
Adedeji1 <afolabiadedeji110@yahoo.com>
2025-09-02 06:42:10 +0100

    fixes to testimonial

--- app/testimonial/TestimonialCard.tsx
+++ app/testimonial/TestimonialCard.tsx
@@ -8,24 +8,9 @@ interface Props {
 }
 
 export default function TestimonialCard({ quote, author, rating, row }: Props) {
-  // Row–based sizing
   const sizeClasses = row === "top" ? "w-[480px] h-[206px]" : "w-[380px] h-[230px]";
 
   return (
-    // <div className={`bg-[#F9C29E] rounded-xl p-4 shadow-md flex flex-col justify-between ${sizeClasses}`}>
-    //   {/* Icon + Quote inline */}
-    //   <div className="flex items-start gap-2">
-    //     {/* replace with your custom imported icon */}
-    //     <Image src="/images/QuoteIcon.svg" alt="quote" width={20} height={20} />
-    //     <p className="text-sm md:text-base ">{quote}</p>
-    //   </div>
-
-    //   {/* Author + Rating */}
-    //   <div className="mt-4">
-    //     <div className="font-semibold">{author}</div>
-    //     <div className="text-black">{"★".repeat(rating)}</div>
-    //   </div>
-    // </div>
     <div className={`bg-[#F9C29E] rounded-xl p-6 shadow-md flex flex-col justify-between ${sizeClasses}`}>
       {/* Top Icon + Quote */}
       <div className="flex items-start gap-2">
