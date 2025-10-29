f9eeee8fc84e3a3eaab7458f6f9087d999599f94
Adedeji1 <afolabiadedeji110@yahoo.com>
2025-10-29 12:28:27 +0100

    Added CTA to the hero section leading to shop

--- components/LandingPage/Hero/hero.tsx
+++ components/LandingPage/Hero/hero.tsx
@@ -10,9 +10,13 @@ import PlayImg from "@/public/images/googleIcon.svg";
 import AppleImg from "@/public/images/AppleImg.svg";
 import Container from "@/components/container/Container";
 import Link from "next/link";
+import { useAppLink } from "@/hooks/getAppLink";
+import JoloLogoWhite from "@/public/images/JoloLogo.svg";
+import DownloadIcon from "@/public/images/hero/download icon.png";
 
 export default function HeroSection() {
   const [animate, setAnimate] = useState(false);
+  const getAppLink = useAppLink();
 
   useEffect(() => {
     const timer = setTimeout(() => setAnimate(true), 100);
@@ -53,33 +57,23 @@ export default function HeroSection() {
               animate ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-6"
             }`}
           >
-            <Badge href="#" text="" className="rounded-lg px-4 py-2">
-              <Link
-                href="https://play.google.com/store/apps/details?id=com.jolojolo.user.app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+            <Badge href="#" text="" className="rounded-lg px-4 py-2 bg-white/10 hover:bg-white/20 transition">
+              <Link href="https://shop.jolojolo.com" target="_blank" rel="noopener noreferrer">
                 <div className="flex items-center">
-                  <Image src={PlayImg} alt="Google Play" className="w-10 h-10 object-contain" />
+                  <Image src={JoloLogoWhite} alt="Web Store" className="w-10 h-10 object-contain" />
                   <div className="flex flex-col leading-tight ml-2">
-                    <span className="text-[10px] text-left ">GET IT ON</span>
-                    <span className="text-base font-semibold -mt-1">Google Play</span>
+                    <span className="text-base font-semibold -mt-1">Shop on the Web</span>
                   </div>
                 </div>
               </Link>
             </Badge>
 
             <Badge href="#" text="" textColor="text-black" className="rounded-md px-4 bg-white">
-              <Link
-                href="https://apps.apple.com/ng/app/jolo-delivery/id6748380014"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
+              <Link href={getAppLink()} target="_blank" rel="noopener noreferrer">
                 <div className="flex items-center">
-                  <Image src={AppleImg} alt="Apple Store" className="w-10 h-10 object-contain" />
+                  <Image src={DownloadIcon} alt="Apple Store" className="w-10 h-10 object-contain" />
                   <div className="flex flex-col leading-tight ml-2">
-                    <span className="text-xs text-left">Download on the</span>
-                    <span className="text-base font-semibold -mt-1">App Store</span>
+                    <span className="text-base font-semibold -mt-1">Download the App</span>
                   </div>
                 </div>
               </Link>
